0d17765bdcdea4beac32be683973ca3ac8e916e9
Student <andrii.kuliak@gmail.com>
2025-04-02 10:36:11 +0300

    Added some conccurency

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+#include <thread>
+
+void thread_func() {
+  int counter = 0;
+  while(counter < 10) {
+    counter++;
+
+    std::this_thread::sleep_for(1000);
+  }
+}
+
 int main()
 {
+  std::cout << "Program has been started" << std::endl;
+
+  std::thread thread(thread_func);
+  thread.join();
+
   return 0;
 }
